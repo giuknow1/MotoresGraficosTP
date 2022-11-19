5a4efbd02431f9ef276c9d6d9f751195a8740cb6
giuknow1 <giulianomacchiavelli03@gmail.com>
2022-11-19 20:19:18 -0300

    Agregadas 2 Flickering Lights al personaje

--- Assets/Scripts/Jugador/JetPack.cs
+++ Assets/Scripts/Jugador/JetPack.cs
@@ -10,7 +10,7 @@ public class JetPack : MonoBehaviour
     public Rigidbody rb;
     public Transform groundedTransform;
     public ParticleSystem effect;
-
+    public ParticleSystem effect2;
     private float c_combust;
 
     void Start()
@@ -33,15 +33,18 @@ public class JetPack : MonoBehaviour
                 c_combust -= Time.deltaTime;
                 rb.AddForce(rb.transform.up * empuje, ForceMode.Impulse);
                 effect.Play();
+                effect2.Play();
             }
             else if (Physics.Raycast(groundedTransform.position, Vector3.down, 0.05f, LayerMask.GetMask("Grounded")) && c_combust < combustible)
             {
                 c_combust += Time.deltaTime;
                 effect.Stop();
+                effect2.Stop();
             }
             else
             {
                 effect.Stop();
+                effect2.Stop();
             }
 
         }
