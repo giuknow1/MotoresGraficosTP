dfdb2286f6af7a3863ec7680264891c2f70e791d
giuknow1 <giulianomacchiavelli03@gmail.com>
2022-11-19 18:29:31 -0300

    Creado Script "JetPack" con la mecánica de vuelo limitado por combustible

---
+++ Assets/Scripts/Jugador/JetPack.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetPack : MonoBehaviour
+{
+
+    public float combustible = 4f;
+    public float empuje = 0.5f;
+    public Rigidbody rb;
+    public Transform groundedTransform;
+    public ParticleSystem effect;
+
+    private float c_combust;
+
+    void Start()
+    {
+        c_combust = combustible;
+    }
+
+ 
+    void Update()
+    {
+        if (c_combust == 0)
+        {
+
+        }
+        else
+        {
+
+            if (Input.GetAxis("Jump") > 0f && c_combust > 0f)
+            {
+                c_combust -= Time.deltaTime;
+                rb.AddForce(rb.transform.up * empuje, ForceMode.Impulse);
+                effect.Play();
+            }
+            else if (Physics.Raycast(groundedTransform.position, Vector3.down, 0.05f, LayerMask.GetMask("Grounded")) && c_combust < combustible)
+            {
+                c_combust += Time.deltaTime;
+                effect.Stop();
+            }
+            else
+            {
+                effect.Stop();
+            }
+
+        }
+    }
+}
