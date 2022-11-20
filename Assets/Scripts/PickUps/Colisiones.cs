afbe74a62aa478037fcc2928134ee9d1252fbbc8
giuknow1 <giulianomacchiavelli03@gmail.com>
2022-11-20 02:36:09 -0300

    Agregados Script "GameManager", "Sonido" y "GestorDeAudio" para reproducir sonidos. Agregados sonidos al recolectar PickUps

--- Assets/Scripts/PickUps/Colisiones.cs
+++ Assets/Scripts/PickUps/Colisiones.cs
@@ -3,37 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Colisiones : MonoBehaviour
+
 {
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Corazon") == true)
         {
             other.gameObject.SetActive(false);
+            GestorDeAudio.instancia.ReproducirSonido("Vida");
         }
 
         if (other.gameObject.CompareTag("Escudo") == true)
         {
             other.gameObject.SetActive(false);
+            GestorDeAudio.instancia.ReproducirSonido("Escudo");
         }
 
         if (other.gameObject.CompareTag("Bateria") == true)
         {
             other.gameObject.SetActive(false);
+            GestorDeAudio.instancia.ReproducirSonido("Bateria");
         }
 
         if (other.gameObject.CompareTag("Bateria Grande") == true)
         {
             other.gameObject.SetActive(false);
+            GestorDeAudio.instancia.ReproducirSonido("Gran Bateria");
         }
 
         if (other.gameObject.CompareTag("Energia Mala") == true)
         {
             other.gameObject.SetActive(false);
+            GestorDeAudio.instancia.ReproducirSonido("Bateria Mala");
         }
 
         if (other.gameObject.CompareTag("Moneda") == true)
         {
             other.gameObject.SetActive(false);
+            GestorDeAudio.instancia.ReproducirSonido("Moneda");
         }
 
 
